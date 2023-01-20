60ddc17bbc1519b6fdf94105a1cf72587885d96d
thierry.leveque <thierry.leveque@alithya.com>
2023-01-20 13:18:24 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IpcRenderer } from 'electron';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'electron-api-test';
+
+  private ipc?: IpcRenderer
+  electronApp = false
+  constructor() {
+    if ((<any>window).require) {
+      try {
+        this.ipc = (<any>window).require('electron').ipcRenderer;
+        this.electronApp = true
+
+        this.ipc?.on('open-signal', (event, arg) => {
+          console.log('Signal received: ', event)
+          console.log('Args: ', arg)
+        })
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      console.warn('App not running inside Electron!');
+    }
+  }
+
+  openModal(){
+    console.log("Open a modal");
+    this.ipc?.send("openModal");
+  }
 }
